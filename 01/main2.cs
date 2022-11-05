9953bc33900bdedd1262d88c9470a0d34439a1ca
kento <kento@toriwatari.bird>
2022-11-06 08:51:33 +0900

    ✨ Update

--- 01/main2.cs
+++
@@ -1,9 +0,0 @@
-class Hello2
-{
-    static void Main()
-    {
-        System.Console.WriteLine(100 + 5);
-        System.Console.WriteLine(100 * 2);
-        System.Console.WriteLine(100 / 3);
-    }
-}
